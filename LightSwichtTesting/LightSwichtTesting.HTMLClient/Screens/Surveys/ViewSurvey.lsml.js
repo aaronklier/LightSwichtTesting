023642748829bfc4955294c1c6c08630e4df9e8a
aaronklier <aaron.klier@outlook.com>
2015-02-17 21:12:56 +0100

    added files and code for multiple datepicker, without logic for adding dates as question

--- LightSwichtTesting/LightSwichtTesting.HTMLClient/Screens/Surveys/ViewSurvey.lsml.js
+++ LightSwichtTesting/LightSwichtTesting.HTMLClient/Screens/Surveys/ViewSurvey.lsml.js
@@ -8,3 +8,29 @@ myapp.ViewSurvey.Details_postRender = function (element, contentItem) {
     });
 }
 
+myapp.ViewSurvey.ScreenContent_render = function (element, contentItem) {
+    
+    $(element).addClass("multiDatePicker");
+    $('.multiDatePicker').multiDatesPicker({
+        numberOfMonths: [3, 4]
+    });
+
+};
+
+myapp.ViewSurvey.SaveDates_postRender = function (element, contentItem) {
+    $(element).css("float","right");
+};
+
+myapp.ViewSurvey.SaveDates_execute = function (screen) {
+
+    var dates = $('.multiDatePicker').multiDatesPicker('getDates');
+    if(dates.length === 0) {
+        alert("Please pick your dates!");
+        return;
+    }
+
+    $.each(dates, function (key, item) {
+        alert(key + " : " + item);
+    });
+
+};
